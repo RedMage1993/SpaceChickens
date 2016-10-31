f4c2c0bf93adf6864351513d025e03add6e98c8b
RedMage1993 <redmage1993@gmail.com>
2016-10-31 16:51:18 -0700

    basic game screen manager

---
+++ SpaceChickens/GameScreenManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+
+namespace SpaceChickens
+{
+    class GameScreenManager
+    {
+        public Graphics graphics { get; private set; }
+        public Stack<IGameScreen> screens { get; private set; }
+
+        public GameScreenManager(Graphics g)
+        {
+            graphics = g;
+        }
+
+        public void push(IGameScreen screen)
+        {
+            screens.Push(screen);
+        }
+
+        public IGameScreen top()
+        {
+            return screens.Peek();
+        }
+    }
+}
