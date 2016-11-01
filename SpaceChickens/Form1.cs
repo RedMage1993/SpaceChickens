e96de81c3b921255a613ba820f61bd40767e72d9
RedMage1993 <redmage1993@gmail.com>
2016-10-31 18:25:14 -0700

    removed border; centered window; fix directbitmap namespace; add worker; add picturebox; grab graphics object and pass into new gsm object

--- SpaceChickens/Form1.cs
+++ SpaceChickens/Form1.cs
@@ -11,9 +11,40 @@ namespace SpaceChickens
 {
     public partial class SpaceChickens : Form
     {
+        private DirectBitmap dbmp;
+        private Graphics graphics;
+        private GameScreenManager gsm;
+
         public SpaceChickens()
         {
             InitializeComponent();
+
+            dbmp = new DirectBitmap(pbxGame.Width, pbxGame.Height);
+            graphics = Graphics.FromImage(dbmp.Bitmap);
+            gsm = new GameScreenManager(graphics);
+        }
+
+        private void SpaceChickens_Shown(object sender, System.EventArgs e)
+        {
+            Application.DoEvents();
+
+            bgwDrawing.RunWorkerAsync();
+        }
+
+        private void bgwDrawing_DoWork(object sender, DoWorkEventArgs e)
+        {
+            // Push Start Menu into gsm
+            // Update
+            // Render
+            // Invalidate
+        }
+
+        private void bgwDrawing_RunWorkerCompleted(object sender, System.ComponentModel.RunWorkerCompletedEventArgs e)
+        {
+            graphics.Dispose();
+            dbmp.Dispose();
+
+            Application.Exit();
         }
     }
 }
